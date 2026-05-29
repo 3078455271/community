4672cc68ebdab000188f0560f69cf5dff4ff5d37
3078455271 <3078455271@qq.com>
2026-05-29 09:49:41 +0800

    fix: 移除 CategoryService 中对不存在的 CategoryVO 的无用导入

--- backend/src/main/java/xyz/haimianxiaozi/service/CategoryService.java
+++ backend/src/main/java/xyz/haimianxiaozi/service/CategoryService.java
@@ -2,7 +2,6 @@ package xyz.haimianxiaozi.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import xyz.haimianxiaozi.entity.Category;
-import xyz.haimianxiaozi.vo.CategoryVO;
 
 import java.util.List;
 
