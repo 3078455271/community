c060ee6d2f570d3abf7295b14748fa2ebe0cf3e1
3078455271 <3078455271@qq.com>
2026-05-29 11:47:03 +0800

    fix: resolve post service compile error

--- backend/src/main/java/xyz/haimianxiaozi/service/PostServiceExt.java
+++ backend/src/main/java/xyz/haimianxiaozi/service/PostServiceExt.java
@@ -1,20 +1,13 @@
 package xyz.haimianxiaozi.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import xyz.haimianxiaozi.entity.Post;
 import xyz.haimianxiaozi.vo.PostVO;
 
-public interface PostServiceExt {
+public interface PostServiceExt extends IService<Post> {
 
     Page<PostVO> getPostPage(int page, int size, Long categoryId);
 
     PostVO getPostDetail(Long id);
-
-    void save(Post post);
-
-    Post getById(Long id);
-
-    void updateById(Post post);
-
-    void removeById(Long id);
 }
